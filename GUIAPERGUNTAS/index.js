c0ba553eb424527fb867081ff870fb5d08979bc0
Carlos Eduardo Souza <88462882+CarlosELSouza@users.noreply.github.com>
2022-05-11 22:21:44 -0300

    11/05

--- GUIAPERGUNTAS/index.js
+++ GUIAPERGUNTAS/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const app = express() //criando uma instância do express
 
 app.set('view engine', 'ejs') // Falando pro express, que o motor HTML (quem vai montar a página) que eu vou usar é o EJS
 
@@ -7,6 +7,15 @@ app.listen(3000, ()=> {
     console.log('App rodando')
 })
 
-app.get('/', (req, res) =>{
-    res.render('index') // Vou montar a tela usando o EJS, função render do express (NÃO PRECISA DO .EJS, O RENDER OLHA DIRETO NA PASTA VIEWS)
+app.get('/:nome/:lang', (req, res) =>{
+    var nome = req.params.nome
+    var lang = req.params.lang
+    var exibirMsg = true
+
+    res.render('index', {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do programador",
+        msg: exibirMsg
+    }) // Vou montar a tela usando o EJS, função render do express (NÃO PRECISA DO .EJS, O RENDER OLHA DIRETO NA PASTA VIEWS)
 })
